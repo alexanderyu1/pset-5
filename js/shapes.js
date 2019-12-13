a26d0690d7dfe8952c03faff5b8493fa8f241d5b
Alex Yu <alex@NEBULA.milkyway.net>
2019-12-13 00:00:18 -0500

    second commit

--- js/shapes.js
+++ js/shapes.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+verifywindow.onload = function() {
     document.getElementById("hello-sample").onclick = sayHelloStaff;
     document.getElementById("rectangle-sample").onclick = drawRectangleStaff;
     document.getElementById("colored-rectangle-sample").onclick = drawColoredRectangleStaff;
@@ -38,19 +38,18 @@ const sayHello = function() {
   character.clearRect(0, 0, 1024, 128);
 
   exerciseOneInput = prompt("Message:");
-    if (text.length >= 50) {
+    if (exerciseOneInput.length >= 50) {
       alert("Your message is too long. Keep it under 50 characters.");
       exerciseOneInput = prompt("Message:");
     }
-    else if (text === null) {
+    else if (exerciseOneInput === null) {
       character = studentCanvas1.getContext("2d");
       character.clearRect(0, 0, 1024, 128);
   }
 
-  character.clearRect(0, 0, 1024, 128);
   character.font = '48px sans-serif';
-  character.strokeText(text, 30, 70);
-  
+  character.clearRect(0, 0, 1024, 128);
+  character.strokeText(exerciseOneInput, 30, 70);
 };
 
 /*
@@ -58,7 +57,45 @@ const sayHello = function() {
  */
 
 const drawRectangle = function() {
-    // write your exercise 2 code here
+
+    let studentCanvas2 = document.getElementById('student-canvas-2');
+    let character2 = studentCanvas2.getContext('2d');
+    character2.clearRect(0, 0, 1024, 128);
+
+    do {
+    var width = prompt("Width: ");
+    var height = prompt("Height: ");
+    var x = prompt("X: ");
+    var y = prompt("Y: ");
+
+    if (width == null || height == null || x == null || y == null) {
+      character2.clearRect(0, 0, 1024, 128);
+      break;
+    }
+    if (width > 1024 || width < 1) { //stuff for width
+      alert("Your width must be between 1 and 1024.")
+    }
+    else if (height > 512 || height < 1) {//stuff for height
+      alert("Your height must be between 1 and 512.")
+    }
+    else if (x < 1 || x > 1024) {//stuff for x
+      alert("Your x-coordinate must be between 1 and 1024.")
+    }
+    else if (y < 1 || y > 512) {//stuff for y
+      alert("Your y-coordinate must be between 1 and 512.")
+    }
+    else if (isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y)) {//stuff for isnan
+      alert("One of your values is not a number.")
+    }
+    else if (Number(width) + Number(x) > 1024 || Number(height) + Number(y) > 512) {//stuff for too big
+      alert("Your rectangle won't fit on the canvas.")
+    }
+  } while(x < 1 || x > 1024 || y < 1 || y > 512 || isNaN(width) || isNaN(height) ||
+          isNaN(x) || isNaN(y) || Number(width) + Number(x) > 1024 || Number(height) + Number(y) > 512
+          || width > 1024 || width < 1 || height > 512 || height < 1)
+
+    character2.clearRect(0, 0, 1024, 512);
+    character2.strokeRect(x, y, width, height);
 };
 
 /*
@@ -66,7 +103,62 @@ const drawRectangle = function() {
  */
 
 const drawColoredRectangle = function() {
-    // write your exercise 3 code here
+
+    let studentCanvas3 = document.getElementById("student-canvas-3");
+    let character3 = studentCanvas3.getContext("2d");
+    character3.clearRect(0, 0, studentCanvas3.width, studentCanvas3.height);
+
+    let verify = true;
+    let input = null;
+
+    while (verify == true) {
+      input = prompt("Color: ");
+      input = input.toLowerCase();
+      input = input.trim();
+
+      switch (input) {
+        case "black":
+          character3.fillStyle = input;
+          verify = false;
+          break;
+        case "blue":
+          character3.fillStyle = input;
+          verify = false;
+          break;
+        case "orange":
+          character3.fillStyle = input;
+          verify = false;
+          break;
+        case "green":
+          character3.fillStyle = input;
+          verify = false;
+          break;
+        case "purple":
+          character3.fillStyle = input;
+          verify = false;
+          break;
+        case "yellow":
+          character3.fillStyle = input;
+          verify = false;
+          break;
+        case "red":
+          character3.fillStyle = input;
+          verify = false;
+          break;
+        case null:
+          verify = false;
+          break;
+        default:
+          alert("That is not a supported color. Try again.");
+          break;
+      }
+      }
+
+      if (input != null) {
+        character3.beginPath();
+        character3.rect(10, 10, 100, 50);
+        character3.fill();
+      }
 };
 
 /*
@@ -74,7 +166,7 @@ const drawColoredRectangle = function() {
  */
 
 const drawTriangle = function() {
-    // write your exercise 4 code here
+
 };
 
 /*
