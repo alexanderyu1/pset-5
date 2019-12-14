ecb368eeba3e44dc05cd4a79c85d5b441ffd313f
Alex Yu <alex@NEBULA.milkyway.net>
2019-12-14 15:01:10 -0500

    awefa

--- js/shapes.js
+++ js/shapes.js
@@ -1,22 +1,10 @@
-verifywindow.onload = function() {
+window.onload = function() {
     document.getElementById("hello-sample").onclick = sayHelloStaff;
     document.getElementById("rectangle-sample").onclick = drawRectangleStaff;
     document.getElementById("colored-rectangle-sample").onclick = drawColoredRectangleStaff;
     document.getElementById("triangle-sample").onclick = drawTriangleStaff;
     document.getElementById("smile-sample").onclick = drawFaceStaff;
     document.getElementById("pyramid-sample").onclick = drawPyramidStaff;
-
-    // this is how we're connecting our buttons to our JavaScript functions. let's walk through it.
-    //
-    // document.getElementById("some-id")   <-- you need to give each button a unique ID
-    //                                          and access it in this manner
-    //
-    // onclick is an event that is fired when you click something (in our case, a button).
-    // when we give onclick a value, we're telling JavaScript what to do when we click the button.
-    // you should set onclick equal to your function names (i.e., sayHello).
-    //
-    // there are six event listeners being added for the staff solutions. you'll have an
-    // equivalent set of six event listeners for your solutions. the first one is done for you.
 
     document.getElementById("hello").onclick = sayHello;
     document.getElementById("rectangle").onclick = drawRectangle;
@@ -31,7 +19,6 @@ verifywindow.onload = function() {
  */
 
 const sayHello = function() {
-
   let exerciseOneInput = "";
   let studentCanvas1 = document.getElementById('student-canvas-1');
   let character = studentCanvas1.getContext("2d");
@@ -57,7 +44,6 @@ const sayHello = function() {
  */
 
 const drawRectangle = function() {
-
     let studentCanvas2 = document.getElementById('student-canvas-2');
     let character2 = studentCanvas2.getContext('2d');
     character2.clearRect(0, 0, 1024, 128);
@@ -90,7 +76,7 @@ const drawRectangle = function() {
     else if (Number(width) + Number(x) > 1024 || Number(height) + Number(y) > 512) {//stuff for too big
       alert("Your rectangle won't fit on the canvas.")
     }
-  } while(x < 1 || x > 1024 || y < 1 || y > 512 || isNaN(width) || isNaN(height) ||
+    } while(x < 1 || x > 1024 || y < 1 || y > 512 || isNaN(width) || isNaN(height) ||
           isNaN(x) || isNaN(y) || Number(width) + Number(x) > 1024 || Number(height) + Number(y) > 512
           || width > 1024 || width < 1 || height > 512 || height < 1)
 
@@ -103,7 +89,6 @@ const drawRectangle = function() {
  */
 
 const drawColoredRectangle = function() {
-
     let studentCanvas3 = document.getElementById("student-canvas-3");
     let character3 = studentCanvas3.getContext("2d");
     character3.clearRect(0, 0, studentCanvas3.width, studentCanvas3.height);
@@ -116,41 +101,41 @@ const drawColoredRectangle = function() {
       input = input.toLowerCase();
       input = input.trim();
 
-      switch (input) {
-        case "black":
-          character3.fillStyle = input;
-          verify = false;
-          break;
-        case "blue":
-          character3.fillStyle = input;
-          verify = false;
-          break;
-        case "orange":
-          character3.fillStyle = input;
-          verify = false;
-          break;
-        case "green":
-          character3.fillStyle = input;
-          verify = false;
-          break;
-        case "purple":
-          character3.fillStyle = input;
-          verify = false;
-          break;
-        case "yellow":
-          character3.fillStyle = input;
-          verify = false;
-          break;
-        case "red":
-          character3.fillStyle = input;
-          verify = false;
-          break;
-        case null:
-          verify = false;
-          break;
-        default:
-          alert("That is not a supported color. Try again.");
-          break;
+    switch (input) {
+      case "black":
+        character3.fillStyle = input;
+        verify = false;
+        break;
+      case "blue":
+        character3.fillStyle = input;
+        verify = false;
+        break;
+      case "orange":
+        character3.fillStyle = input;
+        verify = false;
+        break;
+      case "green":
+        character3.fillStyle = input;
+        verify = false;
+        break;
+      case "purple":
+        character3.fillStyle = input;
+        verify = false;
+        break;
+      case "yellow":
+        character3.fillStyle = input;
+        verify = false;
+        break;
+      case "red":
+        character3.fillStyle = input;
+        verify = false;
+        break;
+      case null:
+        verify = false;
+        break;
+      default:
+        alert("That is not a supported color. Try again.");
+        break;
       }
       }
 
@@ -166,7 +151,60 @@ const drawColoredRectangle = function() {
  */
 
 const drawTriangle = function() {
+  let studentCanvas4 = document.getElementById("student-canvas-4");
+  let character4 = studentCanvas4.getContext("2d");
+  character4.clearRect(0, 0, studentCanvas4.width, studentCanvas4.height);
 
+  let verify = true;
+
+  while (verify == true) {
+    let aSide = prompt("side 1:");
+    let bSide = prompt("side 2:");
+    let cSide = prompt("side 3:");
+
+    if (cSide >= bSide) {
+      cSide = cSide
+    }
+    else {
+      cSide = bSide
+    }
+
+    if (cSide >= aSide){
+      cSide = cSide
+    }
+    else {
+      cSide = aSide
+    }
+
+    if (bSide >= aSide) {
+      bSide = bSide
+    }
+    else {
+      bSide = aSide
+    }
+
+    if (isNaN(aSide) == true || isNaN(bSide) == true || isNaN(cSide) == true) {
+      alert("One of your sides was not a number.");
+    }
+    else if (cSide == null || bSide == null || aSide == null) {
+      verify = false;
+    }
+    else if ((25 + Number(bSide)) > 1024 || (Number(aSide) + 25) > 512) {
+      alert("Your triangle won't fit on the canvas.");
+    }
+    else if (cSide ** 2 != aSide ** 2 + bSide ** 2) {
+      alert("That's not a valid right triangle.");
+    }
+    else {
+      character4.beginPath();
+      character4.moveTo(25, 25);
+      character4.lineTo(25, (25+Number(aSide)));
+      character4.lineTo((25 + Number(bSide)), (25+Number(aSide)));
+      character4.lineTo(25, 25);
+      character4.stroke();
+      verify = false;
+  }
+  }
 };
 
 /*
